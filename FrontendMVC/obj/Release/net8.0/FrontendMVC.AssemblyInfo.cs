4aad0926c392608cf6583cfb1c364ebc9a4616e6
permanaanggy99 <permanaanggy99@gmail.com>
2024-08-27 22:59:34 +0700

    initial

--- FrontendMVC/obj/Release/net8.0/FrontendMVC.AssemblyInfo.cs
+++ FrontendMVC/obj/Release/net8.0/FrontendMVC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontendMVC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d295a65cdf670f389cefbdea34d804eb744112")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontendMVC")]
